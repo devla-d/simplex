4be501720f1bf75d488f5fa5c70ad56e0ae7319c
samuel aniekan <samuelaniekan680@gmail.com>
2023-07-14 16:25:03 +0100

    modified settings

--- baseapp/settings.py
+++ baseapp/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = "assets/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "assets"),)
-STATIC_ROOT = "/home/simplext/public_html/assets"
+STATIC_ROOT = "/home4/simplext/public_html/assets"
 
 MEDIA_URL = "/media/"
 
